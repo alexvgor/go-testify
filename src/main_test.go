d3ae66ed71c97f7d86f4843d508642bcf34e55ae
Aleksey Gordeev <avgordeev72@gmail.com>
2024-05-28 22:47:18 +0300

    fixes

--- src/main_test.go
+++ src/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -14,42 +13,66 @@ import (
 func TestMainHandlerWhenOk(t *testing.T) {
 	// запрос к сервису
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
 	responseRecorder := httptest.NewRecorder()
+
 	handler := http.HandlerFunc(mainHandle)
+
 	handler.ServeHTTP(responseRecorder, req)
+
 	// необходимые проверки
 	status := responseRecorder.Code
-	assert.Equal(t, http.StatusOK, status, fmt.Sprintf("expected status code: %d, got %d", http.StatusOK, status))
+
+	assert.Equal(t, http.StatusOK, status)
+
 	assert.NotEmpty(t, responseRecorder.Body)
 }
 
 func TestMainHandlerWhenUnknownCity(t *testing.T) {
 	// запрос к сервису
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=tagil", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=Tuevokukuevo", nil)
+
 	responseRecorder := httptest.NewRecorder()
+
 	handler := http.HandlerFunc(mainHandle)
+
 	handler.ServeHTTP(responseRecorder, req)
+
 	// проверка статуса
 	status := responseRecorder.Code
-	assert.Equal(t, status, http.StatusBadRequest, fmt.Sprintf("expected status code: %d, got %d", http.StatusBadRequest, status))
+
+	assert.Equal(t, status, http.StatusBadRequest)
+
 	// проверка тела ответа
 	require.NotEmpty(t, responseRecorder.Body)
+
 	body := responseRecorder.Body.String()
-	assert.Equal(t, "wrong city value", body, fmt.Sprintf("expected response body: %s, got %s", "wrong city value", body))
+
+	assert.Equal(t, "wrong city value", body)
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+
 	totalCount := 4
+
 	// запрос к сервису
 	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
+
 	responseRecorder := httptest.NewRecorder()
+
 	handler := http.HandlerFunc(mainHandle)
+
 	handler.ServeHTTP(responseRecorder, req)
+
 	// проверка статуса
 	status := responseRecorder.Code
-	assert.Equal(t, http.StatusOK, status, fmt.Sprintf("expected status code: %d, got %d", http.StatusOK, status))
+
+	assert.Equal(t, http.StatusOK, status)
+
 	// проверка тела ответа
 	require.NotEmpty(t, responseRecorder.Body)
+
 	body := responseRecorder.Body.String()
+
 	assert.Len(t, strings.Split(body, ","), totalCount)
 }
